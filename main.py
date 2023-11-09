28c607fef44134562ac18603f18dfbaffb506e43
Hubert26 <69253428+Hubert26@users.noreply.github.com>
2023-11-09 18:13:04 +0100

    Update main.py
    scatter_plot in progress

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.io as pio
 import seaborn as sns
+from IPython.display import display
 import glob
 import os
 from scipy.interpolate import interp1d
@@ -98,23 +99,55 @@ def find_indx(df, **kwargs):
     return search_res.index.tolist()
   
 #%%
+
+    
+#%%
 def find_serie(df, indx, start=0, stop=100000):
     result = []
     maximum = []
+    minimum = []
     
     for i in indx:
         serie = df.loc[i].dropna().astype('int')
         suma = serie.cumsum()
         max = suma.iloc[-1]
+        min = suma.iloc[0]
         if stop > max:
             stop = max
 
         selected_columns = [column for column in suma.index if (start < suma[column]) & (suma[column] < stop)]
-        result.append(serie[selected_columns].values.tolist())
-        result.append(suma[selected_columns].values.tolist())
+        result.append((serie[selected_columns].values.tolist(), suma[selected_columns].values.tolist()))
         maximum.append(max)
+        minimum.append(min)
     
-    return result, maximum
+    return result, minimum, maximum
+
+
+#%%
+def scatter_plot(tuple_list, info_list_of_lists, title=''):
+    stop = max(sublist[-1] for sublist in info_list_of_lists)
+    start = min(sublist[-2] for sublist in info_list_of_lists)
+    
+    fig = px.scatter()
+    
+    for i in range(len(tuple_list)):
+        name = ' '.join(map(str, info_list_of_lists[i]))
+        
+        fig.add_scatter(
+            x=tuple_list[i][0],
+            y=tuple_list[i][1],
+            mode='markers',
+            name=name
+        )
+        
+    fig.update_layout(
+        xaxis_title="Time [ms]",
+        yaxis_title="Time Between Heartbeats [ms]",
+        title=f"{title} RANGE from {start} to {stop}"
+    )
+    display(fig)
+    #pio.write_html(fig, f"{title}, RANGE from {start} to {stop}.html")
+
 #%%
 file_paths = glob.glob('data/**')
 
@@ -128,11 +161,27 @@ for i in range(len(file_paths)):
 indx = find_indx(metadata_df, NUMBER = meas_num, PAIR = meas_pair, TYPE = meas_type)
 
 #%%
-serie, maximum = find_serie(data_df, indx)
+serie, minimum, maximum = find_serie(data_df, indx)
 
+#%%
+info_df = metadata_df.loc[indx]
+info_df['Min'] = minimum
+info_df['Max'] = maximum
+info_list_of_lists = info_df.values.tolist()
 
+#%%
+scatter_plot(serie, info_list_of_lists, title = 'TEST')
 
+#%%
 
+x = [1, 2, 3, 4, 5]
+y = [10, 15, 13, 17, 20]
+
+# Utwórz wykres liniowy
+fig = px.line(x=x, y=y, labels={'x': 'X-Axis', 'y': 'Y-Axis'}, title='Prosty Wykres Liniowy')
+
+# Wyświetl wykres
+fig.show()
 
 
 
